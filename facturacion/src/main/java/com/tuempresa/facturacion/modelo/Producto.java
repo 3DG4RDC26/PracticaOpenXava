897352a76ff4bc53d4d8b83c515f04d89830f17a
3DG4RDC26 <ercasco@uamv.edu.ni>
2025-11-03 15:57:13 -0600

    Clase Producto

---
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Producto.java
@@ -0,0 +1,20 @@
+package com.tuempresa.facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Producto {
+    @Id
+    @Column(length = 9)
+    int numero;
+
+    @Column(length = 50)
+    String descripcion;
+}
